24ad55386e0140ff72e5b82b1d68728de05f8676
Karmhh <carmelapia01@gmail.com>
2024-07-09 17:25:48 +0200

    [ Delete ] fix

--- yt_app_fe/public/yt_dw_App/app.py
+++ yt_app_fe/public/yt_dw_App/app.py
@@ -1,3 +1,5 @@
+import logging
+import uuid
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from pytube import YouTube
@@ -29,27 +31,35 @@ async def uploadVideo(request: VideoObj):
     info = yt_dlp.YoutubeDL({'quiet':True ,'extract_flat':True}).extract_info(request.video_url,download=False)
     title = info.get('title',None)
     video = YouTube(request.video_url)
-    arrayVideos.append({"title":title,"video":video,"ex":request.ex,"url":request.video_url, "index": len(arrayVideos)})
+    arrayVideos.append({"title":title,"video":video,"ex":request.ex,"url":request.video_url, "index": uuid.uuid4()})
     return arrayVideos
 
 @app.get("/videos")
 async def uploadVideo():
       return arrayVideos
-
+# Configure logging
+logging.basicConfig(level=logging.INFO)
 
 @app.get("/delete/{index}")
-async def delete_video(index: int):
-    # check if the index is valid
-    if index >= len(arrayVideos):
+async def delete_video(index: str):
+    logging.info(f"Received request to delete video with index: {index}")
+    logging.info(f"Current videos: {arrayVideos}")
+    
+    # Find the video object with the specified index property
+    video = next((video for video in arrayVideos if video.get("index") == uuid.UUID(index)), None)
+
+    # Check if the video object was found
+    if video is None:
+        logging.error(f"Video with index {index} not found")
         raise HTTPException(status_code=404, detail="Video not found")
 
-    # find the video object at the specified index
-    video = arrayVideos[index]
+    # Remove the video object from the array
+    arrayVideos.remove(video)
 
-    # remove the video object from the array
-    arrayVideos.pop(index)
+    # Log success
+    logging.info(f"Video {video['title']} deleted successfully")
 
-    # return a success message
+    # Return a success message
     return {"message": f"Video {video['title']} deleted successfully"}
 
 @app.get("/download")
@@ -65,5 +75,4 @@ async def download_video():
         arrayVideos.clear()         
         return {"message": "Download Completed."}
     else:
-        return {"message": "No Videos Uploaded."}
-
+        raise HTTPException(status_code=404, detail="No Videos Uploaded.") 
--- yt_app_fe/src/App.js
+++ yt_app_fe/src/App.js
@@ -1,77 +1,115 @@
-import './App.css';
+import "./App.css";
 
-import 'primeicons/primeicons.css';
+import "primeicons/primeicons.css";
 
-import { Button } from 'primereact/button';
-import { InputText } from 'primereact/inputtext';
-import { Dropdown } from 'primereact/dropdown';
-import { Toast } from 'primereact/toast';
-import "./index.css"
-import { useState, useRef, useEffect } from 'react';
-import axios from 'axios';
-import { Stepper } from 'primereact/stepper';
-import { StepperPanel } from 'primereact/stepperpanel';
-import { Splitter, SplitterPanel } from 'primereact/splitter';
-import { DataScroller } from 'primereact/datascroller';
-import { Tag } from 'primereact/tag';
+import "/node_modules/primeflex/primeflex.css";
+import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
+import { Dropdown } from "primereact/dropdown";
+import { Toast } from "primereact/toast";
+import "./index.css";
+import { useState, useRef, useEffect } from "react";
+import axios from "axios";
+import { Stepper } from "primereact/stepper";
+import { StepperPanel } from "primereact/stepperpanel";
+import { Splitter, SplitterPanel } from "primereact/splitter";
+import { DataScroller } from "primereact/datascroller";
+import { Tag } from "primereact/tag";
 
 function App() {
   const toast = useRef(null);
   const [selectedOption, setSelectedOption] = useState(null);
   const [url, setUrl] = useState("");
   const options = [
-    { name: 'Video', code: '.mp4' },
-    { name: 'Audio Only', code: '.mp3' },
+    { name: "Video", code: ".mp4" },
+    { name: "Audio Only", code: ".mp3" },
   ];
 
   const handleSubmit = async () => {
-    const urlDestination = `http://127.0.0.1:8000/download`
+    const urlDestination = `http://127.0.0.1:8000/download`;
     try {
       const response = await axios.get(urlDestination);
 
-      if (response.status === 200) {
-        setlistVideos([])
-        toast.current.show({ severity: 'success', summary: 'Success', detail: `${response.data.message}`, life: 3000 });
-      } else {
-        toast.current.show({ severity: 'error', summary: 'Error', detail: `${response.data.message}`, life: 3000 });
-      }
+        setlistVideos([]);
+        toast.current.show({
+          severity: "success",
+          summary: "Success",
+          detail: `${response.data.message}`,
+          life: 3000,
+        });
+ 
     } catch (error) {
-      console.error(error);
-      toast.current.show({ severity: 'error', summary: 'Error', detail: error, life: 3000 });
-
-    }
-
-  }
+    
+        console.error(error); toast.current.show({
+          severity: "error",
+          summary: "Error",
+          detail: `${error.response.data.detail}`,
+          life: 3000,
+        });
+      }
+  };
   const stepperRef = useRef(null);
-  const [listVideos, setlistVideos] = useState([])
+  const [listVideos, setlistVideos] = useState([]);
 
   const itemTemplate = (data) => {
     return (
-      <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between", margin: "1rem" }}>
-        <div style={{ display: "block", width: "80%" }}>
-          <h4 style={{ width:"80%" }}>
+      <div
+        className="grid"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <div className="col-10 grid ">
+          <h4 className="col-12">
             <i>{data.title}</i>
-            <Tag style={{marginLeft:"3rem"}} value={data.ex} severity={"info"}></Tag>
           </h4>
-          <Tag icon="pi pi-chevron-right" value={data.url} severity={"help"}></Tag>
+
+          <Tag
+            className="col-10"
+            icon="pi pi-chevron-right"
+            value={data.url}
+            severity={"help"}
+          />
+          <Tag className="col-2" value={data.ex} severity={"info"} />
         </div>
-
-        <Button icon="pi pi-times" rounded outlined severity="danger" aria-label="Cancel" onClick={async () => await axios.get(`http://127.0.0.1:8000/delete/${data.index}`).then(resp => toast.current.show({ severity: 'success', summary: 'Success', detail: `${resp.data.message}`, life: 3000 }), setlistVideos([]))} />
-        <hr />
+        <div className="col-2">
+          <Button
+            icon="pi pi-times"
+            rounded
+            outlined
+            severity="danger"
+            aria-label="Cancel"
+            onClick={async () =>
+              await axios
+                .get(`http://127.0.0.1:8000/delete/${data.index}`)
+                .then(
+                  (resp) =>
+                    toast.current.show({
+                      severity: "success",
+                      summary: "Success",
+                      detail: `${resp.data.message}`,
+                      life: 3000,
+                    }),fetchData()
+                )
+            }
+          />
+        </div>
       </div>
-
-
     );
   };
-
-  useEffect(() => {
-    async function fetchData() {
+ async function fetchData() {
       try {
-        await axios.get("http://127.0.0.1:8000/videos").then(resp => setlistVideos(resp.data))
+        await axios
+          .get("http://127.0.0.1:8000/videos")
+          .then((resp) => setlistVideos(resp.data));
       } catch (error) {
         console.log(error.message);
       }
     }
+  useEffect(() => {
+   
     fetchData();
   }, []);
   return (
@@ -79,60 +117,212 @@ function App() {
       <header className="App-header">
         <i>YouTube Downloader</i>
       </header>
-      <div style={{ padding: "1rem" }} >
+      <div className="card grid lg:hidden">
+        <div className=" col-12 sm:col-12 lg:col-6 ">
+          <h2>
+            <i>Upload Videos</i>
+          </h2>
+          <Stepper
+            ref={stepperRef}
+            style={{ margin: "1rem" }}
+            orientation="vertical"
+          >
+            <StepperPanel header="Type your Url">
+              <div className="   border-round justify-content-center align-items-center font-medium">
+                <InputText
+                  style={{ width: "100%", margin: "1rem" }}
+                  value={url}
+                  onChange={(e) => setUrl(e.target.value)}
+                />
+              </div>
 
+              <Button
+                outlined
+                label="Next"
+                icon="pi pi-arrow-right"
+                iconPos="right"
+                severity="info"
+                onClick={() => stepperRef.current.nextCallback()}
+              />
+            </StepperPanel>
+            <StepperPanel header="Chose the format">
+              <Dropdown
+                style={{ width: "100%" }}
+                value={selectedOption}
+                onChange={(e) => setSelectedOption(e.value)}
+                options={options}
+                optionLabel="name"
+                placeholder="Select format"
+              />
 
-        <Splitter style={{ height: '85vh' }}>
-          <SplitterPanel className="flex align-items-center justify-content-center">
-            <div className="card">
-              <h2><i>Upload Videos</i></h2>
-              <Stepper ref={stepperRef} style={{ flexBasis: '50rem', margin: "2rem" }} orientation="vertical">
-                <StepperPanel header="Type your Url">
-                  <div className="flex flex-column h-12rem">
-                    <div className="border-2 border-dashed surface-border border-round surface-ground flex-auto flex justify-content-center align-items-center font-medium">   <InputText style={{ width: "50%", margin: "1rem" }} value={url} onChange={(e) => setUrl(e.target.value)} /></div>
-                  </div>
-                  <div className="flex py-4">
-                    <Button style={{ margin: "1rem" }} outlined label="Next" icon="pi pi-arrow-right" iconPos="right" severity='info' onClick={() => stepperRef.current.nextCallback()} />
-                  </div>
-                </StepperPanel>
-                <StepperPanel header="Chose the format">
-                  <div className="flex flex-column h-12rem ">
-                    <div className="border-2 border-dashed surface-border border-round surface-ground flex-auto flex justify-content-center align-items-center font-medium"><Dropdown style={{ width: "50%", margin: "1rem" }} value={selectedOption} onChange={(e) => setSelectedOption(e.value)} options={options} optionLabel="name"
-                      placeholder="Select format" className="w-full md:w-14rem" />
-                      <div className="flex py-4 gap-2"></div>
+              <div>
+                <Button
+                  outlined
+                  style={{ margin: "1rem" }}
+                  label="Back"
+                  severity="secondary"
+                  icon="pi pi-arrow-left"
+                  onClick={() => stepperRef.current.prevCallback()}
+                />
+                <Button
+                  outlined
+                  style={{ margin: "1rem" }}
+                  label="Upload"
+                  icon="pi pi-upload"
+                  iconPos="right"
+                  onClick={async () => url!=="" && selectedOption !== null ?
+                    await axios
+                      .post("http://127.0.0.1:8000/uploadVideo", {
+                        video_url: url,
+                        ex: selectedOption.code,
+                      })
+                      .then((resp) => setlistVideos(resp.data), setUrl("")): (
+                         toast.current.show({
+                          severity: "error",
+                          summary: "Error",
+                          detail: `No Url or Format detected, Please fill the field.`,
+                          life: 3000,
+                        })
+                      )
+                  }
+                />
+              </div>
+            </StepperPanel>
+          </Stepper>
+        </div>
+        <div className=" col-12 sm:col-12 lg:col-6 ">
+          <h2>
+            <i>My Videos</i>
+          </h2>
+          <div style={{ height: "35vh", overflow: "auto" }}>
+            <DataScroller
+              value={listVideos}
+              itemTemplate={itemTemplate}
+              rows={50}
+              buffer={0.4}
+            />
+          </div>
+          <Button
+            outlined
+            style={{ margin: "1rem" }}
+            label="Download"
+            severity="info"
+            position={"right"}
+            icon="pi pi-download"
+            onClick={handleSubmit}
+          />
+        </div>
+      </div>
+      <div className="hidden lg:block">
+        <Splitter style={{ height: "85vh" }}>
+          <SplitterPanel className="flex align-items-center justify-content-center ">
+            <div className="card ">
+              <h2>
+                <i>Upload Videos</i>
+              </h2>
+              <div style={{ height: "60vh" }}>
+                <Stepper ref={stepperRef} orientation="vertical">
+                  <StepperPanel header="Type your Url">
+                    <div className="flex-auto flex justify-content-center align-items-center font-medium">
+                      <InputText
+                        style={{ width: "100%" }}
+                        value={url}
+                        onChange={(e) => setUrl(e.target.value)}
+                      />
                     </div>
-                    <Button outlined style={{ margin: "1rem" }} label="Back" severity="secondary" icon="pi pi-arrow-left" onClick={() => stepperRef.current.prevCallback()} />
-                    <Button outlined style={{ margin: "1rem" }} label="Upload" icon="pi pi-upload" iconPos="right" onClick={async () =>
-                      await axios.post("http://127.0.0.1:8000/uploadVideo", {
-                        "video_url": url,
-                        "ex": selectedOption.code
-                      }).then(resp => setlistVideos(resp.data), setUrl(""))
 
-                    } />
-                  </div>
-                </StepperPanel>
-
-              </Stepper>
-
+                    <div className="flex py-4">
+                      <Button
+                        outlined
+                        label="Next"
+                        icon="pi pi-arrow-right"
+                        iconPos="right"
+                        severity="info"
+                        onClick={() => stepperRef.current.nextCallback()}
+                      />
+                    </div>
+                  </StepperPanel>
+                  <StepperPanel header="Chose the format">
+                    <div className="flex flex-column ">
+                      <div className="flex-auto flex justify-content-center align-items-center font-medium">
+                        <Dropdown
+                          value={selectedOption}
+                          onChange={(e) => setSelectedOption(e.value)}
+                          options={options}
+                          optionLabel="name"
+                          placeholder="Select format"
+                          className="w-full"
+                        />
+                      </div>
+                      <div className="flex py-2 ">
+                        <Button
+                          outlined
+                          style={{ margin: "1rem" }}
+                          label="Back"
+                          severity="secondary"
+                          icon="pi pi-arrow-left"
+                          onClick={() => stepperRef.current.prevCallback()}
+                        />
+                        <Button
+                          outlined
+                          style={{ margin: "1rem" }}
+                          label="Upload"
+                          icon="pi pi-upload"
+                          iconPos="right"
+                          onClick={async () => url!=="" && selectedOption !== null ?
+                            await axios
+                              .post("http://127.0.0.1:8000/uploadVideo", {
+                                video_url: url,
+                                ex: selectedOption.code,
+                              })
+                              .then((resp) => setlistVideos(resp.data), setUrl("")): (
+                                 toast.current.show({
+                                  severity: "error",
+                                  summary: "Error",
+                                  detail: `No Url or Format detected, Please fill the field.`,
+                                  life: 3000,
+                                })
+                              )
+                          }
+                        />
+                      </div>
+                    </div>
+                  </StepperPanel>
+                </Stepper>
+              </div>
             </div>
           </SplitterPanel>
+
           <SplitterPanel className="flex align-items-center justify-content-center ">
-
-            <div className='card' >
-              <h2><i>My Videos</i></h2>
-              <div style={{ height: "60vh", overflow: "auto" }}>
-                <DataScroller value={listVideos} itemTemplate={itemTemplate} rows={50} buffer={0.4} />
+            <div className="card">
+              <h2>
+                <i>My Videos</i>
+              </h2>
+              <div style={{ height: "50vh", overflow: "auto" }}>
+                <DataScroller
+                  value={listVideos}
+                  itemTemplate={itemTemplate}
+                  rows={50}
+                  buffer={0.4}
+                />
               </div>
-              <Button outlined style={{ margin: "1rem" }} label="Download" severity="info" position={"right"} icon="pi pi-download" onClick={handleSubmit} />
-
+              <Button
+                outlined
+                style={{ margin: "1rem" }}
+                label="Download"
+                severity="info"
+                position={"right"}
+                icon="pi pi-download"
+                onClick={handleSubmit}
+              />
             </div>
           </SplitterPanel>
         </Splitter>
 
-        <Toast ref={toast} position='center' />
+        <Toast ref={toast} position="center" />
       </div>
+      <Toast ref={toast} position="center" />
     </div>
-
   );
 }
 
